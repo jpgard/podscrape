060cc58c857af91e9b94659d482bff6448d0d615
jpgard <joshua.patrick.gardner@gmail.com>
2023-07-02 10:34:23 -0400

    handle connection exceptions; allow to resume from file

--- get_urls.py
+++ get_urls.py
@@ -1,6 +1,6 @@
 """
 Usage:
-python get_urls.py --api-key YOUR_API_KEY --api-secret YOUR_API_SECRET
+python get_urls.py --resume-from-outfile
 
 """
 import argparse
@@ -9,7 +9,9 @@ import json
 
 import pandas as pd
 import podcastindex
+from requests.exceptions import ConnectionError
 from tqdm import tqdm
+from urllib3.exceptions import ProtocolError
 
 
 def main(
@@ -18,8 +20,9 @@ def main(
         api_key: str = None,
         api_secret: str = None,
         max_episodes_per_feed=10_000,
+        resume_from_outfile: bool = False,
         outfile="episodes.csv"):
-    episode_data = []
+    current_data = []
     if api_key and api_secret:
         config = {
             "api_key": "YOUR API KEY HERE",
@@ -29,56 +32,78 @@ def main(
         with open("keys.json", "r") as f:
             config = json.load(f)
 
+    if resume_from_outfile:
+        archived_data = pd.read_csv(outfile)
+        print(f"[INFO] successfully read in {len(archived_data)} records")
+    else:
+        archived_data = None
+
     index = podcastindex.init(config)
     timestamp = datetime.datetime.now()
-    # while len(episode_data) < max_episodes:
-    for feed_id in range(1, max_feed_id):
-        print(f"processing feed %s" % feed_id)
-        feed = index.podcastByFeedId(feed_id).get('feed')
+    feed_id = 1
+    num_retries = 0
+    max_num_retries = 2048
+    while feed_id < max_feed_id:
+        try:
+            print(f"processing feed %s" % feed_id)
+            feed = index.podcastByFeedId(feed_id).get('feed')
 
-        if not feed:
-            print(f'skipping empty feed id %s' % feed_id)
-            continue
+            if not feed:
+                print(f'skipping empty feed id %s' % feed_id)
+                continue
 
-        recent_episodes = index.episodesByFeedId(
-            feed_id,
-            max_results=max_episodes_per_feed,
-            # since=-525600 # in the last year
-        )
-        if not recent_episodes['items']:
-            continue
-        else:
-            print(f"got {len(recent_episodes['items'])} recent episodes"
-                  f"\ncurrent total: {len(episode_data)}")
-        for episode in tqdm(recent_episodes['items']):
-            # results = index.episodeById(1270106072)
-            url = episode['enclosureUrl']
-            episode_data.append(
-                dict(episode_url=url,
-                     episode_title=episode['title'],
-                     episode_date_published_pretty=episode['datePublishedPretty'],
-                     episode_date_published=episode['datePublished'],
-                     duration=episode['duration'],
-                     is_explicit=episode['explicit'],
-                     transcript_url=episode['transcriptUrl'],
-                     enclosureLength=episode['enclosureLength'],
-                     enclosureType=episode['enclosureType'],
-                     enclosureUrl=episode['enclosureUrl'],
-                     episodeType=episode['episodeType'],
-                     feed_id=feed_id,
-                     feed_title=feed['title'],
-                     feed_categories=feed['categories'],
-                     feed_explicit=feed['explicit'],
-                     feed_language=feed['language'],
-                     ))
+            recent_episodes = index.episodesByFeedId(
+                feed_id,
+                max_results=max_episodes_per_feed,
+                # since=-525600 # in the last year
+            )
+            if not recent_episodes['items']:
+                continue
+            else:
+                print(f"got {len(recent_episodes['items'])} recent episodes"
+                      f"\ncurrent total: {len(current_data)}")
+            for episode in tqdm(recent_episodes['items']):
+                # results = index.episodeById(1270106072)
+                url = episode['enclosureUrl']
+                current_data.append(
+                    dict(episode_url=url,
+                         episode_title=episode['title'],
+                         episode_date_published_pretty=episode['datePublishedPretty'],
+                         episode_date_published=episode['datePublished'],
+                         duration=episode['duration'],
+                         is_explicit=episode['explicit'],
+                         transcript_url=episode['transcriptUrl'],
+                         enclosureLength=episode['enclosureLength'],
+                         enclosureType=episode['enclosureType'],
+                         enclosureUrl=episode['enclosureUrl'],
+                         episodeType=episode['episodeType'],
+                         feed_id=feed_id,
+                         feed_title=feed['title'],
+                         feed_categories=feed['categories'],
+                         feed_explicit=feed['explicit'],
+                         feed_language=feed['language'],
+                         ))
 
-        if (datetime.datetime.now() - timestamp).total_seconds() > (15 * 60):
-            timestamp = datetime.datetime.now()
-            print(f"[INFO] writing {len(episode_data)} episodes to {outfile}")
-            pd.DataFrame(episode_data).to_csv(outfile, index=False)
+            if (datetime.datetime.now() - timestamp).total_seconds() > (15 * 60):
+                timestamp = datetime.datetime.now()
+                archived_data = pd.concat((archived_data, pd.DataFrame(current_data)))
+                print(f"[INFO] writing {len(current_data)} new episodes to {outfile}; {len(archived_data)} total")
+                archived_data.to_csv(outfile, index=False)
+                current_data = []
+            if len(archived_data) > max_episodes:
+                print(f"[INFO] terminating with {len(archived_data)} episodes.")
+                break
+        except (ConnectionResetError, ConnectionError, ProtocolError) as e:
+            print(f"[WARNING] got exception {e}; resetting connection")
+            num_retries += 1
+            if num_retries > max_num_retries:
+                break
+            else:
+                index = podcastindex.init(config)
 
-    print(f"[INFO] writing {len(episode_data)} episodes to {outfile}")
-    pd.DataFrame(episode_data).to_csv(outfile, index=False)
+    archived_data = pd.concat((archived_data, pd.DataFrame(current_data)))
+    print(f"[INFO] writing {len(current_data)} new episodes to {outfile}; {len(archived_data)} total")
+    archived_data.to_csv(outfile, index=False)
 
 
 if __name__ == "__main__":
@@ -86,5 +111,7 @@ if __name__ == "__main__":
     parser.add_argument("--api-key", type=str)
     parser.add_argument("--api-secret", type=str)
     parser.add_argument("--max-episodes", type=int, default=1000)
+    parser.add_argument("--resume-from-outfile", type=bool,
+                        default=False, action="store_true")
     args = parser.parse_args()
     main(**vars(args))
